06cb16c18300c7e5da5081c2368eaa9bb28f2785
William PENSEC <william.pensec@univ-ubs.fr>
2024-04-30 10:08:41 +0200

    Update on some code for simulating HC Implem 2 spatial

--- app/src/code_execution.py
+++ app/src/code_execution.py
@@ -340,9 +340,20 @@ set bool_cycle 0
 while {$sim_active == 1} {
     set value_pc [examine -hex /tb/top_i/core_region_i/RISCV_CORE/if_stage_i/pc_id_o]
 
+    set sec_if      [examine -hex /tb/top_i/core_region_i/RISCV_CORE/if_stage_i/sec_interrupt]
+    set sec_id      [examine -hex /tb/top_i/core_region_i/RISCV_CORE/id_stage_i/sec_interrupt]
+    set sec_rf_tag  [examine -hex /tb/top_i/core_region_i/RISCV_CORE/id_stage_i/registers_i_tag/sec_interrupt]
+    set sec_ex      [examine -hex /tb/top_i/core_region_i/RISCV_CORE/ex_stage_i/sec_interrupt]
+    set sec_csr     [examine -hex /tb/top_i/core_region_i/RISCV_CORE/cs_registers_i/sec_interrupt]
+    set sec_lsu     [examine -hex /tb/top_i/core_region_i/RISCV_CORE/load_store_unit_i/sec_interrupt]
+
     #############  CHECKING SIM VALUES #############
     ## if conditions to stop the run cycles
-    if {$nb_cycle > $cycle_ref} {
+    if {[expr {$sec_if} != {"1'h0"}] || [expr {$sec_id} != {"1'h0"}] || [expr {$sec_rf_tag} != {"1'h0"}] || [expr {$sec_ex} != {"1'h0"}] || [expr {$sec_csr} != {"1'h0"}] || [expr {$sec_lsu} != {"1'h0"}]} {
+        ## Detection error ##
+        set status_end 6
+        set sim_active 0
+    } elseif {$nb_cycle > $cycle_ref} {
         ## CYCLE OVERFLOW : CRASH ##
         set sim_active 0
         set status_end 1
